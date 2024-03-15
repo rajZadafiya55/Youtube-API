4cf6b5637bee759c9de0fbf21b528b3f8fc7cd19
Raj Zadafiya <zadafiyaraj395@gmail.com>
2024-03-15 20:11:38 +0530

    publish video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,7 +2,7 @@ import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
-// import {ApiResponse} from "../utils/ApiResponse.js"
+import { ApiResponse } from "../utils/ApiRespone.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
@@ -13,7 +13,51 @@ const getAllVideos = asyncHandler(async (req, res) => {
 
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
-  // TODO: get video, upload to cloudinary, create video
+  if (!title || !description) {
+    throw new ApiError(400, "All Field is required");
+  }
+  //   console.log("req.files video",req.files?.videoFile[0].path)
+  //   console.log("req.files thumbnail ", req.files?.thumbnail[0].path);
+
+  const videoLocalPath = req.files?.videoFile[0].path;
+  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+
+  if (!videoLocalPath) {
+    throw new ApiError(400, "videoFilePath is required");
+  }
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "thumbnailFilePath is required");
+  }
+  //   const vide0Path = req.files?.video
+  //   console.log(title, description);
+
+  const video = await uploadOnCloudinary(videoLocalPath);
+  const thumbnailFile = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!video) {
+    throw new ApiError(400, "video file not found");
+  }
+  if (!thumbnailFile) {
+    throw new ApiError(400, "thumbnail file not found");
+  }
+
+  console.log("video", video);
+  console.log("thumbnail", thumbnailFile);
+
+  const videoUpload = await Video.create({
+    title,
+    description,
+    duration: video.duration,
+    videoFile: video.url,
+    thumbnail: thumbnailFile.url,
+    owner: req.user?._id,
+    isPublished: false,
+  });
+  
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videoUpload, "video uploaded Successfully"));
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
