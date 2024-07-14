161852c5b36680344c020315937754724077d3b5
rajZadafiya55 <zadagfiyaraj395.com>
2024-07-14 17:46:25 +0530

    multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,10 +1,9 @@
 import multer from "multer";
-import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Use path.join to construct the correct path
-    cb(null, "../../public/temp");
+    // cb(null, "../../public/temp");
+    cb(null, "../../public/");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -15,20 +14,3 @@ export const upload = multer({
   storage,
   limits: { fileSize: 50 * 1024 * 1024 },
 });
-
-// import multer from "multer";
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     // cb(null, "../../public/temp");
-//     cb(null, "./public/temp");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// export const upload = multer({
-//   storage,
-//   limits: { fileSize: 50 * 1024 * 1024 },
-// });
