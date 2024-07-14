d5e9eb3405fa286064c4897f75b440c08a6bbd9d
rajZadafiya55 <zadagfiyaraj395.com>
2024-07-14 18:49:39 +0530

    cloudinary

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,22 +1,9 @@
 import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import fs from "fs";
-
-// These lines are necessary to make __dirname available in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const tempDir = path.join(__dirname, "../../public/");
-
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, tempDir);
+    // cb(null, "../../public/temp");
+    cb(null, "./public");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
