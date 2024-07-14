df52b83e348468b6f863e8d6c53db04244ba6cea
rajZadafiya55 <zadagfiyaraj395.com>
2024-07-14 18:38:41 +0530

    multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,9 +1,10 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // cb(null, "../../public/temp");
-    cb(null, "./public");
+    // Use path.join to construct the correct path
+    cb(null, path.join(__dirname, "../../public"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
